0854cc3ad0809129dd35aafec3b188eca971b848
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-10 11:46:16 +0200

    add: toLowerCase function + tests

--- toLowerCase.test.js
+++ toLowerCase.test.js
@@ -1,3 +1,9 @@
+const toLowerCase = (text) => {
+  return text.toLowerCase();
+};
+
 test('La funzione createSlug restituisce una stringa in lowercase.', () => {
   expect(toLowerCase('TESTO MINUSCOLO')).toBe('testo minuscolo');
+  expect(toLowerCase('tEstOaCaSo3234')).toBe('testoacaso3234');
+  expect(toLowerCase('sono già minuscolo')).toBe('sono già minuscolo');
 });
