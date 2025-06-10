ac88f32d02942d1b47dd1c106112ca04c92fd8f2
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-10 11:32:05 +0200

    add: getInitials function + tests

--- getInitials.test.js
+++ getInitials.test.js
@@ -1,5 +1,11 @@
 // snack 1
 
+const getInitials = (fullName) => {
+  const names = fullName.split(' ');
+  return names.reduce((acc, name) => acc + name[0].toUpperCase(), '');
+};
+
 test('La funzione getInitials restituisce le iniziali di un nome completo', () => {
   expect(getInitials('Lorenzo Ferrante')).toBe('LF');
+  expect(getInitials('franco rossi')).toBe('FR');
 });
