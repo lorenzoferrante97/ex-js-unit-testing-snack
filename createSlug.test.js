0e5f73f92518a51f99197be4180beb6a7f9ae521
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-10 17:33:36 +0200

    edit: createSlug tests in describe

--- createSlug.test.js
+++ createSlug.test.js
@@ -7,15 +7,17 @@ beforeEach(() => {
   initialText = 'Testo Slug per Test';
 });
 
-test('La funzione createSlug restituisce una stringa in lowercase.', () => {
-  expect(createSlug('TESTO MINUSCOLO')).toBe('testo-minuscolo');
-  expect(createSlug('sono già minuscolo')).toBe('sono-già-minuscolo');
-});
+describe('creare uno slug', () => {
+  test('La funzione createSlug restituisce una stringa in lowercase.', () => {
+    expect(createSlug('TESTO MINUSCOLO')).toBe('testo-minuscolo');
+    expect(createSlug('sono già minuscolo')).toBe('sono-già-minuscolo');
+  });
 
-test('La funzione createSlug sostituisce gli spazi con -', () => {
-  expect(createSlug(initialText)).toBe('testo-slug-per-test');
-});
+  test('La funzione createSlug sostituisce gli spazi con -', () => {
+    expect(createSlug(initialText)).toBe('testo-slug-per-test');
+  });
 
-test('La funzione createSlug lancia un errore se il titolo è vuoto o non valido', () => {
-  expect(() => createSlug('    ')).toThrow('La stringa non può essere vuota');
+  test('La funzione createSlug lancia un errore se il titolo è vuoto o non valido', () => {
+    expect(() => createSlug('    ')).toThrow('La stringa non può essere vuota');
+  });
 });
