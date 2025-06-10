5b3a6a953e457fdf525fa987ce1b0962e0da0295
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-10 17:03:24 +0200

    add: test findPostById

---
+++ findPostById.test.js
@@ -0,0 +1,9 @@
+test('La funzione findPostById restituisce il post corretto dato l’array di post e l’id', () => {
+  const posts = [
+    { id: 1, title: 'Ciao a tutti', slug: 'ciao-a-tutti' },
+    { id: 2, title: 'Test Prova', slug: 'test-prova' },
+    { id: 3, title: 'Post di Esempio 1', slug: 'post-di-esempio-1' },
+  ];
+
+  expect(findPostById(posts, 2)).toEqual({ id: 2, title: 'Test Prova', slug: 'test-prova' });
+});
