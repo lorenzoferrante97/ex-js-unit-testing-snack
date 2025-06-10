2e9522d5420fe473ea1e42b6c6f0f3e35e66cff4
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-10 12:25:13 +0200

    add: isPalindrome function + tests

--- isPalindrome.test.js
+++ isPalindrome.test.js
@@ -1,3 +1,12 @@
+const isPalindrome = (text) => {
+  const reverseText = text.split('').reverse().join('');
+  console.log(text, reverseText);
+  return text === reverseText;
+};
+
 test('La funzione isPalindrome verifica se una stringa è un palindromo', () => {
-  expect(isPalindrome('osso')).toBe('osso');
+  expect(isPalindrome('osso')).toBeTruthy();
+  expect(isPalindrome('ciao')).toBeFalsy();
+  expect(isPalindrome('osso ciao')).toBeFalsy();
+  expect(isPalindrome('annA osso Anna')).toBeTruthy();
 });
