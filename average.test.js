ece0c8d002ec6ca2598c40f5ed2ec9bd43a4bf14
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-10 11:54:17 +0200

    add: average function

--- average.test.js
+++ average.test.js
@@ -1,3 +1,8 @@
+const average = (nums) => {
+  const sum = nums.reduce((acc, num) => acc + num, 0);
+  return sum / nums.length;
+};
+
 test('La funzione average calcola la media aritmetica di un array di numeri.', () => {
   const numbers = [2, 4, 6, 8, 10];
   expect(average(numbers)).toBe(6);
